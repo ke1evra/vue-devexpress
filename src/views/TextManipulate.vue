4de51d50b65ebb6b7ad26df0609155024657647c
ke1evra <dolgov.konstantin@gmail.com>
2020-02-16 22:05:40 +0300

    текст

--- src/views/TextManipulate.vue
+++ src/views/TextManipulate.vue
@@ -7,48 +7,98 @@
         </div>
       </div>
       <div class="row">
-        <div class="col-4 py-5" v-html="textFromObject(data[0])">
-        </div>
-        <div class="col-4 py-5" v-html="textFromObject(data[63])">
-        </div>
-        <div class="col-4 py-5" v-html="textFromObject(data[180])">
-        </div>
-        <div class="col-4 py-5" v-html="textFromObject(data[120])">
-        </div>
-        <div class="col-4 py-5" v-html="textFromObject(data[499])">
-        </div>
-        <div class="col-4 py-5" v-html="textFromObject(data[315])">
-        </div>
-        <div class="col-4 py-5" v-html="textFromObject(data[410])">
-        </div>
-        <div class="col-4 py-5" v-html="textFromObject(data[315])">
-        </div>
-        <div class="col-4 py-5" v-html="textFromObject(data[225])">
-        </div>
-        <div class="col-4 py-5" v-html="textFromObject(data[35])">
-        </div>
-        <div class="col-4 py-5" v-html="textFromObject(data[45])">
-        </div>
-        <div class="col-4 py-5" v-html="textFromObject(data[145])">
-        </div>
+<!--        <div class="col-4 py-5" v-html="textFromObject(data[0])">-->
+<!--        </div>-->
+<!--        <div class="col-4 py-5" v-html="textFromObject(data[63])">-->
+<!--        </div>-->
+<!--        <div class="col-4 py-5" v-html="textFromObject(data[180])">-->
+<!--        </div>-->
+<!--        <div class="col-4 py-5" v-html="textFromObject(data[120])">-->
+<!--        </div>-->
+<!--        <div class="col-4 py-5" v-html="textFromObject(data[499])">-->
+<!--        </div>-->
+<!--        <div class="col-4 py-5" v-html="textFromObject(data[315])">-->
+<!--        </div>-->
+<!--        <div class="col-4 py-5" v-html="textFromObject(data[410])">-->
+<!--        </div>-->
+<!--        <div class="col-4 py-5" v-html="textFromObject(data[315])">-->
+<!--        </div>-->
+<!--        <div class="col-4 py-5" v-html="textFromObject(data[225])">-->
+<!--        </div>-->
+<!--        <div class="col-4 py-5" v-html="textFromObject(data[35])">-->
+<!--        </div>-->
+<!--        <div class="col-4 py-5" v-html="textFromObject(data[45])">-->
+<!--        </div>-->
+<!--        <div class="col-4 py-5" v-html="textFromObject(data[145])">-->
+<!--        </div>-->
+    </div>
+    <div class="row">
+      <div class="col">
+        <DxDataGrid
+          :show-borders="true"
+          :allow-column-reordering="true"
+          :data-source="constructDataSourse()"
+          :hover-state-enabled="true"
+        >
+          <DxColumn
+            data-field="art_num"
+            caption="Артикул"
+            width="100"
+          ></DxColumn>
+          <DxColumn
+            data-field="name"
+            caption="Наименование"
+          ></DxColumn>
+          <DxColumn
+            data-field="text"
+            caption="Текст"
+            height="1000"
+          ></DxColumn>
+          <DxExport
+            :enabled="true"
+          />
+        </DxDataGrid>
       </div>
     </div>
+  </div>
   </div>
 </template>
 
 <script>
+import {
+  DxDataGrid,
+  DxColumn,
+  // DxGrouping,
+  // DxGroupPanel,
+  // DxSearchPanel,
+  // DxPaging,
+  // DxFormat,
+  // DxPager,
+  // DxSummary,
+  // DxGroupItem,
+  // DxSortByGroupSummaryInfo,
+  // DxTotalItem,
+  // DxFilterRow,
+  // DxHeaderFilter,
+  DxExport,
+
+} from 'devextreme-vue/data-grid';
+import { locale, loadMessages } from 'devextreme/localization';
+import ruMessages from 'devextreme/localization/messages/ru.json';
 
 const data = require('../assets/text.json');
 
 const insertProp = property => (property || '');
 const insertPropWithText = (property, text) => (property ? `${text}: ${property}` : '');
-const concatProps = propsArray => propsArray.reduce((acc, item, i) => {
-  const lineEnding = propsArray.length - 1 === i ? '.' : ';';
-  if (item) {
-    return `${acc} - ${item}${lineEnding}<br/>`;
-  }
-  return acc;
-}, '');
+const concatProps = propsArray => propsArray
+  .reduce((acc, item, i) => {
+    const lineEnding = propsArray.length - 1 === i ? '.' : ';';
+    if (item) {
+      return `${acc} - ${item}${lineEnding}
+`;
+    }
+    return acc;
+  }, '');
 
 
 export default {
@@ -58,53 +108,68 @@ export default {
       data,
     };
   },
+  components: {
+    DxColumn,
+    // DxGroupPanel,
+    // DxGrouping,
+    // DxPaging,
+    // DxSearchPanel,
+    DxDataGrid,
+    // DxFormat,
+    // DxPager,
+    // DxSummary,
+    // DxGroupItem,
+    // DxSortByGroupSummaryInfo,
+    // DxTotalItem,
+    // DxFilterRow,
+    // DxHeaderFilter,
+    DxExport,
+
+  },
+  created() {
+    loadMessages(ruMessages);
+    locale('ru');
+  },
   methods: {
     textFromObject(item) {
       const props = concatProps([
         insertPropWithText(item.seriya, 'Серия'),
-        insertPropWithText(item['pokrytie-fasada'], 'Покрытие фасада'),
+        // insertPropWithText(item['pokrytie-fasada'], 'Покрытие фасада'),
         insertPropWithText(item['napravlenie-otkryvaniya-fasada'], 'Направление открытия фасада'),
         insertPropWithText(item['tip-produkta'], 'Тип продукта'),
-        insertPropWithText(item['czvetovaya-palitra'], 'Цвет'),
-        insertPropWithText(item.pokrytie, 'Покрытие'),
+        // insertPropWithText(item.pokrytie, 'Покрытие'),
         insertPropWithText(item.effekt, 'Эффект'),
-        insertPropWithText(item['tip-drevesiny'], 'Тип древесины'),
+        // insertPropWithText(item['tip-drevesiny'], 'Тип древесины'),
         insertPropWithText(item['krepezh-v-komplekte'], 'Крепеж в комплекте'),
+        insertPropWithText(item['czvetovaya-palitra'], 'Цвет'),
       ]);
       const props2 = concatProps([
         insertPropWithText(item['strana-proizvodstva'], 'Страна производства'),
         insertPropWithText(`${item['shirina-(sm)'] * 10}х${item['vysota-(sm)'] * 10}х${item['tolshhina-(mm)']} мм`, 'Габаритные размеры'),
       ]);
-
+      // ${insertProp(item['pokrytie-vnutrennej-poverxnosti'])}
       return `
-      ### [ВЫБЕРИТЕ И СПРОЕКТИРУЙТЕ КУХНЮ](https://leroymerlin.ru/proekt-kuhni/)
-      **${item['polnoe-naimenovanie-tovara-dlya-klienta']}**
-      ${item['polnoe-naimenovanie-tovara-dlya-klienta'].toString().toLowerCase().includes('фальшпанель') ? ' — декоративная панель для отделки кухонного гарнитура.' : ''}
-      ${item['polnoe-naimenovanie-tovara-dlya-klienta'].toString().toLowerCase().includes('дверь') ? ' защищает содержимое шкафов от пыли, задает внешний вид кухонного гарнитура.' : ''}
-      ${item['polnoe-naimenovanie-tovara-dlya-klienta'].toString().toLowerCase().includes('угол') ? ' — важная составная часть кухонного гарнитура.' : ''}
-      ${insertProp(item['samostoyatelnaya-sborka'])}
-      Основной материал - ${insertProp(item['osnovnoj-material'])}.
-      ${insertProp(item['pokrytie-vnutrennej-poverxnosti'])}
-      ${insertProp(item['sootvetstvie-vnutrennej-storone'])}
-      ${insertProp(item['predvaritelnoe-sverlenie-fasada'])}
-      Delinia — это новая коллекция кухонь Леруа Мерлен. Все товары проходят строгую проверку качества на соответствие стандартам. Это гарантирует прочность и долговечность вашего кухонного гарнитура.
-      <br/>
-      <br/>
-      Советы по обслуживанию: ${insertProp(item['sovety-po-obsluzhivaniyu'])}
-      <br/>
-      <br/>
-      ### Особенности:
-      <br/>
-      <br/>
-      ${props}
-      <br/>
-      ### Основные параметры:
-      <br/>
-      <br/>
-      ${props2}
-      <br/>
-      ${insertProp(item['tip-produkta'])} Delinia ID — одна из составных деталей кухонного гарнитура. Не является самостоятельным продуктом. Петли и ручки в комплект не входят. Для составления полного проекта кухни рекомендуем проконсультироваться с экспертом в гипермаркете «Леруа Мерлен». В торговом зале вы увидите реальный цвет, сможете оценить качество, сравнить разные модели, спроектировать кухню, а также подобрать дополнительные элементы к ней. На любые вопросы вам ответят опытные консультанты. Также перед визитом в магазин вы можете воспользоваться нашим онлайн-конструктором и составить предварительный проект своей кухни. Создайте кухню мечты вместе с «Леруа Мерлен»!
-      `;
+### [ВЫБЕРИТЕ И СПРОЕКТИРУЙТЕ КУХНЮ](https://leroymerlin.ru/proekt-kuhni/)
+**${item['polnoe-naimenovanie-tovara-dlya-klienta']}**${item['polnoe-naimenovanie-tovara-dlya-klienta'].toString().toLowerCase().includes('фальшпанель') ? ' — декоративная панель для отделки кухонного гарнитура.' : ''}${item['polnoe-naimenovanie-tovara-dlya-klienta'].toString().toLowerCase().includes('дверь') ? ' защищает содержимое шкафов от пыли, задает внешний вид кухонного гарнитура.' : ''}${item['polnoe-naimenovanie-tovara-dlya-klienta'].toString().toLowerCase().includes('угол') ? ' — важная составная часть кухонного гарнитура.' : ''} ${insertProp(item['samostoyatelnaya-sborka'])} Основной материал - ${insertProp(item['osnovnoj-material'])}. ${insertProp(item['sootvetstvie-vnutrennej-storone'])} ${insertProp(item['predvaritelnoe-sverlenie-fasada'])}
+Delinia ID — это новая коллекция кухонь Леруа Мерлен. Все товары проходят строгую проверку качества на соответствие стандартам. Это гарантирует прочность и долговечность вашего кухонного гарнитура.
+
+Советы по уходу: Протирать влажной неабразивной тканью или микрофиброй.
+
+### Особенности:
+
+${props}
+### Основные параметры:
+
+${props2}
+${insertProp(item['tip-produkta'])} Delinia ID — одна из составных деталей кухонного гарнитура. Не является самостоятельным продуктом. Петли и ручки в комплект не входят. Для составления полного проекта кухни рекомендуем проконсультироваться с экспертом в гипермаркете «Леруа Мерлен». В торговом зале вы увидите реальный цвет, сможете оценить качество, сравнить разные модели, спроектировать кухню, а также подобрать дополнительные элементы к ней. На любые вопросы вам ответят опытные консультанты. Также перед визитом в магазин вы можете воспользоваться нашим онлайн-конструктором и составить предварительный проект своей кухни. Создайте кухню мечты вместе с «Леруа Мерлен»!
+`.replace(/\v\v+/g, ' ').trim();
+    },
+    constructDataSourse() {
+      return this.data.map(item => ({
+        art_num: item['artikul-lm'],
+        name: item['polnoe-naimenovanie-tovara-dlya-klienta'],
+        text: this.textFromObject(item),
+      }));
     },
   },
 };
