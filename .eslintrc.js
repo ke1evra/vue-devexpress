3f48a16a3c7404fb39609f5e3d7d586c2239f0c9
E123 <E123wh@mail.ru>
2021-05-12 14:18:01 +0300

    обновил eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     "indent": ["error", 4],
+    "linebreak-style": 0,
   },
   parserOptions: {
     parser: 'babel-eslint',
