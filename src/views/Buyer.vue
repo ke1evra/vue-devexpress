c2e62c9400ced6f2e2a313fea189381b1f6160d7
kortess <kortess1999@mail.ru>
2021-05-17 17:52:54 +0300

    Первая часть парсинга отчёта для байера

--- src/views/Buyer.vue
+++ src/views/Buyer.vue
@@ -11,6 +11,7 @@
 </template>
 <script>
 import axios from 'axios';
+import moment from 'moment';
 import BuyersTable from '../components/buyersTable.vue';
 import DualDatePicker from '../components/dualDatePicker.vue';
 import { API_URL } from '../config';
@@ -34,7 +35,44 @@ export default {
             axios.get(url).then((response) => {
                 this.loading = false;
                 this.dataSource = response.data;
+                const dates = [];
+                while (range.from <= range.to) {
+                    dates.push(range.from);
+                    // eslint-disable-next-line no-param-reassign
+                    range.from = moment(range.from).subtract(-1, 'day').format('YYYY-MM-DD');
+                }
+                console.log(this.parseData(response.data, dates));
             }).catch(e => console.log(e));
+        },
+        parseData(data, dates) {
+            const preTableObj = {};
+            // Составление таблицы по товарам
+            // eslint-disable-next-line no-plusplus
+            for (let i = 0; i < data.length; i++) {
+                // eslint-disable-next-line no-prototype-builtins
+                if (!preTableObj.hasOwnProperty(data[i].id)) {
+                    preTableObj[data[i].id] = data[i];
+                    preTableObj[data[i].id].dates = {};
+                    // eslint-disable-next-line no-plusplus
+                    for (let j = 0; j < dates.length; j++) preTableObj[data[i].id].dates[dates[j]] = {};
+                    preTableObj[data[i].id].total_sum = 0;
+                    preTableObj[data[i].id].total_cnt = 0;
+                    preTableObj[data[i].id].total_discount = 0;
+                }
+                // Подсчёт статистики
+                preTableObj[data[i].id].total_sum += preTableObj[data[i].id].sum_price;
+                preTableObj[data[i].id].total_cnt += preTableObj[data[i].id].cnt_sold;
+                preTableObj[data[i].id].total_discount += preTableObj[data[i].id].sum_discount;
+                preTableObj[data[i].id].dates[moment(data[i].ordered_at).format('YYYY-MM-DD')] = {
+                    cnt_sold: data[i].cnt_sold,
+                    sum_price: data[i].sum_price,
+                    sum_discount: data[i].sum_discount,
+                    avg_discount: data[i].avg_discount,
+                };
+            }
+            // eslint-disable-next-line
+            for (let item in preTableObj) preTableObj[item].avg_sum = preTableObj[item].total_sum / preTableObj[item].total_cnt;
+            return Object.values(preTableObj);
         },
     },
 };
